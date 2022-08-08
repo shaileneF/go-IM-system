e5324c4a230c356a2ca34fa5552349f54a97a36d
chenyang389 <chenyang389@jd.com>
2022-08-08 16:22:57 +0800

    用户上线及广播功能

--- src/server.go
+++ src/server.go
@@ -3,11 +3,19 @@ package main
 import (
 	"fmt"
 	"net"
+	"sync"
 )
 
 type Server struct {
 	Ip   string
 	Port int
+
+	// 在线用户的列表
+	OnlineMap map[string]*User
+	mapLock   sync.RWMutex
+
+	// 消息广播的channel
+	Message chan string
 }
 
 // 类似于Java的构造函数
@@ -15,12 +23,41 @@ func NewServer(ip string, port int) *Server {
 	server := new(Server)
 	server.Ip = ip
 	server.Port = port
+	server.OnlineMap = make(map[string]*User)
+	server.Message = make(chan string)
 	return server
 }
 
+func (server *Server) BroadCast(user *User, msg string) {
+	sendMsg := "[" + user.Addr + "]" + user.Name + ": " + msg
+	server.Message <- sendMsg
+}
+
+// 监听Message广播消息channel的方法，用协程执行，一旦有消息就发送给全部的在线user
+func (server *Server) ListenMessage() {
+	for {
+		// 不断尝试从这个message管道中读数据，一旦有消息，做后续处理
+		// 否则会阻塞在这里，这个管道也是无缓冲的，若读不到数据，就是发送方未发送数据，那么接收方会阻塞
+		msg := <-server.Message
+		// 将msg发送给全部的在线User
+		server.mapLock.Lock()
+		for _, user := range server.OnlineMap {
+			user.C <- msg
+		}
+		server.mapLock.Unlock()
+	}
+}
 func (server *Server) Handler(conn net.Conn) {
 	//业务
-	fmt.Println("接收到客户端的连接请求")
+	fmt.Println("接收到客户端的连接请求", conn)
+	//用户上线，将用户加入到onlineMap中
+	user := NewUser(conn)
+	server.mapLock.Lock()
+	server.OnlineMap[user.Name] = user
+	server.mapLock.Unlock()
+	// 广播当前用户上线消息
+	server.BroadCast(user, "已上线")
+	select {}
 }
 
 //启动服务器的函数
@@ -33,8 +70,11 @@ func (server *Server) Start() {
 	}
 	defer listener.Close()
 
+	//启动监听Message的goroutine
+	go server.ListenMessage()
 	for {
 		// accept
+		// accept之后，代表有客户端在连接此server，说明用户上线
 		conn, err := listener.Accept()
 		if err != nil {
 			fmt.Println("listener accept err:", err)
---
+++ src/user.go
@@ -0,0 +1,36 @@
+package main
+
+import "net"
+
+// 每个user都会启动一个goroutine，不断监控这个channel，这个channel用于传递消息
+// 若监控到这个c里面有消息了，便进行之后的业务处理
+// 可以看到在构造方法中，这个c是无缓冲的，说明协程一直监听这个c，若获取不到消息，则会一直阻塞在这里，直到获取到消息
+// 对于无缓冲的channel，接收方会阻塞，直到发送方准备好，发送方会阻塞，直到接收方准备好。
+type User struct {
+	Name string
+	Addr string
+	C    chan string // 这个信道用来接收消息
+	conn net.Conn
+}
+
+// 构造方法
+func NewUser(conn net.Conn) *User {
+	userAddr := conn.RemoteAddr().String()
+	user := new(User)
+	user.Name = userAddr
+	user.Addr = userAddr
+	user.C = make(chan string)
+	user.conn = conn
+	// 启动监听当前user channel消息的goroutine
+	go user.ListenMessage()
+	return user
+}
+
+// 监听当前User channel的方法，一旦有消息，就直接发送给对应客户端
+func (user *User) ListenMessage() {
+	for {
+		// 一定要有数据，否则会阻塞，这个信道是无缓冲的
+		msg := <-user.C
+		user.conn.Write([]byte(msg + "\n"))
+	}
+}
