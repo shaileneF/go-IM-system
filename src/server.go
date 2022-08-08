0c168fe532f25e3dea864e8cd2a2a8c5f33d0169
chenyang389 <chenyang389@jd.com>
2022-08-08 14:50:45 +0800

    构建基础server

--- src/server.go
+++ src/server.go
@@ -1,13 +1,46 @@
 package main
 
+import (
+	"fmt"
+	"net"
+)
+
 type Server struct {
 	Ip   string
 	Port int
 }
 
+// 类似于Java的构造函数
 func NewServer(ip string, port int) *Server {
 	server := new(Server)
 	server.Ip = ip
 	server.Port = port
 	return server
+}
+
+func (server *Server) Handler(conn net.Conn) {
+	//业务
+	fmt.Println("接收到客户端的连接请求")
+}
+
+//启动服务器的函数
+func (server *Server) Start() {
+	// socket listen
+	listener, err := net.Listen("tcp", fmt.Sprintf("%s:%d", server.Ip, server.Port))
+	if err != nil {
+		fmt.Println("net.Listen err:", err)
+		return
+	}
+	defer listener.Close()
+
+	for {
+		// accept
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println("listener accept err:", err)
+			continue
+		}
+		// do handler
+		go server.Handler(conn)
+	}
 }
